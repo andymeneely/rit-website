0c23652c7e550fd4c6f510329b4d7e2ca5ed54b0
Andy Meneely <andy@se.rit.edu>
2015-06-03 12:58:28 -0400

    Having more fun with word clouds

--- js/toggle.js
+++ js/toggle.js
@@ -8,3 +8,23 @@ function toggle(id){
 			e.style.display = 'block';
 	} //else just do nothing because old browsers are dumb...
 }
+
+function toggle_pubcloud(id){
+	toggle(id + "_wordcloud");
+	show_pubcloud(id);
+}
+
+function show_pubcloud(id){
+	if(WordCloud.isSupported){
+		var canvas = document.getElementById(id + "_canvas");
+		canvas.width = 600;
+		canvas.height = 400;
+		WordCloud(canvas, {
+			list: MeneelyESEM2013_wordfreq,
+			gridSize: 1,
+			weightFactor: 0.8,
+			minRotation: 0,
+			maxRotation: 0.78, /* pi/4 */
+		} );
+	}
+}
